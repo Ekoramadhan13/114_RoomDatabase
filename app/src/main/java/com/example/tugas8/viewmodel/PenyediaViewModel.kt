6deace632245176013ec28dfdb2d5bc8fb8885c1
Ekoramadhan13 <enugraha411@gmail.com>
2025-11-27 19:09:12 +0700

    menambahkan isi file PenyediaViewModel.kt

--- app/src/main/java/com/example/tugas8/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/tugas8/viewmodel/PenyediaViewModel.kt
@@ -1,2 +1,30 @@
 package com.example.tugas8.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.tugas8.repositori.AplikasiSiswa
+import com.example.tugas8.viewmodel.EntryViewModel
+import com.example.tugas8.viewmodel.HomeViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
+/**
+ * Fungsi ekstensi query untuk objek [Application] dan mengembalikan sebuah
+ * instance dari
+ * [AplikasiSiswa].
+ */
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
