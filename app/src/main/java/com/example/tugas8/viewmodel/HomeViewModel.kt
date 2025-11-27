d1fb6953793cfbbeac2c0b2ba5f531e7bfe099d3
Ekoramadhan13 <enugraha411@gmail.com>
2025-11-27 16:42:12 +0700

    menambahkan isi data file HomeViewModel.kt

--- app/src/main/java/com/example/tugas8/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/tugas8/viewmodel/HomeViewModel.kt
@@ -1,4 +1,30 @@
 package com.example.tugas8.viewmodel
 
-class HomeViewModel {
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tugas8.repositori.RepositoriSiswa
+import com.example.tugas8.room.Siswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa) : ViewModel() {
+
+    companion object{
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    val homeUiState: StateFlow<HomeUiState> = repositoriSiswa.getAllSiswaStream()
+        .filterNotNull()
+        .map { HomeUiState(listSiswa = it.toList()) }
+        .stateIn(scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(stopTimeoutMillis = TIMEOUT_MILLIS),
+            initialValue = HomeUiState())
+
+    data class HomeUiState(
+        val listSiswa: List<Siswa> = listOf()
+    )
 }
